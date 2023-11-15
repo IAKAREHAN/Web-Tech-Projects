4a6d5a767b4f30da075b698fb5914ddae3ad7f68
Mohd Rehan <90393015+iakarehan@users.noreply.github.com>
2023-11-15 16:33:17 +0530

    Add files via upload

---
+++ Web Tech Projects/typescript/op.ts
@@ -0,0 +1,13 @@
+// Function to perform arithmetic operations
+function performArithmeticOperations(a: number, b: number): void {
+  console.log(`Addition: ${a + b}`);
+  console.log(`Subtraction: ${a - b}`);
+  console.log(`Multiplication: ${a * b}`);
+  console.log(`Division: ${a / b}`);
+}
+
+// Test the function
+const num1: number = 10;
+const num2: number = 5;
+
+performArithmeticOperations(num1, num2);
